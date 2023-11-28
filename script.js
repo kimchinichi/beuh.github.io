080577e5a45ea0954831f20cba8d609a94534a0a
kimchinichi <152254653+kimchinichi@users.noreply.github.com>
2023-11-27 23:50:28 -0700

    script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "Yay, see you on the 19th! 💖";
+  gif.src ="https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  noBtn.style.display = "none";
+  const remainingBtn = document.querySelector('.btn-group button:not(.no-btn)');
+    // Center the remaining button by setting margin to auto
+    remainingBtn.style.margin = 'auto';
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+  noBtn.style.transition = "all 2s"
+});
